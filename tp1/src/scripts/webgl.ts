0d78a0a97c9738d4ac77cdd6e92eef0d00528007
gabokatta <gkatta@fi.uba.ar>
2023-04-24 12:34:11 -0300

    Todo: animate test sphere.

--- tp1/src/scripts/webgl.ts
+++ tp1/src/scripts/webgl.ts
@@ -7,9 +7,17 @@ class WebGL {
     public canvas: HTMLCanvasElement;
     public gl: WebGLRenderingContext;
     public program: WebGLProgram;
+    public modelMatrix: mat4;
+    public viewMatrix: mat4;
+    public projMatrix: mat4;
+    public normalMatrix: mat4;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
+        this.modelMatrix = mat4.create();
+        this.viewMatrix = mat4.create();
+        this.projMatrix = mat4.create();
+        this.normalMatrix = mat4.create();
 
         if (!canvas) throw Error("Your browser does not support WebGL");
         this.gl = canvas.getContext("webgl")!;
@@ -23,29 +31,28 @@ class WebGL {
         this.gl.clearColor(0.1, 0.1, 0.2, 1.0);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-        this.setUpMatrices();
-        this.setUpShaders(vertexShaderPath, fragmentShaderPath);
     };
+
+    async init() {
+        await this.setUpShaders(vertexShaderPath, fragmentShaderPath);
+        this.setUpMatrices();
+        this.setUpWebGL();
+    }
 
     setUpMatrices() {
-        var modelMatrix: mat4 = mat4.create();
-        var viewMatrix: mat4 = mat4.create();
-        var projMatrix: mat4 = mat4.create();
-        var normalMatrix: mat4 = mat4.create();
-
-        mat4.perspective(projMatrix,45, this.canvas.width / this.canvas.height, 0.1, 100.0);
-        mat4.identity(modelMatrix);
-        mat4.rotate(modelMatrix,modelMatrix, -1.57078, [1.0, 0.0, 0.0]);
-        mat4.identity(viewMatrix);
-        mat4.translate(viewMatrix,viewMatrix, [0.0, 0.0, -5.0]);
-        this.setUpVertexShaderMatrices(projMatrix,viewMatrix, modelMatrix, normalMatrix, viewMatrix);
+        mat4.perspective(this.projMatrix,45, this.canvas.width / this.canvas.height, 0.1, 100.0);
+        mat4.identity(this.modelMatrix);
+        mat4.rotate(this.modelMatrix, this.modelMatrix, -1.57078, [1.0, 0.0, 0.0]);
+        mat4.identity(this.viewMatrix);
+        mat4.translate(this.viewMatrix, this.viewMatrix, [0.0, 0.0, -5.0]);
+        this.setMatrixUniforms();
     };
 
-    setUpVertexShaderMatrices(projMatrix: mat4, viewMatrix: mat4, modelMatrix: mat4, normalMatrix: mat4, viewMatrix1: mat4) {
-        this.setMatrix("modelMatrix", modelMatrix);
-        this.setMatrix("viewMatrix", viewMatrix);
-        this.setMatrix("normalMatrix", normalMatrix);
-        this.setMatrix("projMatrix", projMatrix);
+    setMatrixUniforms() {
+        this.setMatrix("modelMatrix", this.modelMatrix);
+        this.setMatrix("viewMatrix", this.viewMatrix);
+        this.setMatrix("normalMatrix", this.normalMatrix);
+        this.setMatrix("projMatrix", this.projMatrix);
     }
 
     async setUpShaders(vertexFile: URL, fragmentFile: URL) {
@@ -68,8 +75,8 @@ class WebGL {
         this.gl.useProgram(this.program);
     }
 
-    draw(vertex: Array<number>, index: Array<number>, normals: Array<number>, method: DrawMethod) {
-
+    draw(vertex: Array<number>, index: Array<number>, normals: Array<number>, method: DrawMethod = DrawMethod.Smooth) {
+        this.setMatrixUniforms();
         const vertexBuffer = this.createBuffer(vertex);
         const normalBuffer = this.createBuffer(normals);
         const indexBuffer = this.createIndexBuffer(index);
@@ -99,6 +106,8 @@ class WebGL {
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
         this.gl.vertexAttribPointer(attributeLocation, size, this.gl.FLOAT, false, 0 , 0); 
     }
+
+    //TODO: Try to replicate the demo via animating the sphere updating the matrices with setMatrix().
 
     setMatrix(name: string, matrix: mat4) {
         var matrixUniform: WebGLUniformLocation = this.gl.getUniformLocation(this.program, name)!;
