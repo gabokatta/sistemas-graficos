7af2400f5f830f1770b2f6012c332465ec5ba0c6
Snowpack Build <actions@github.com>
2023-04-28 15:16:29 +0000

    Commit from GitHub Actions (Snowpack)

--- tp1/build/dist/demos/cylinder-demo.js
+++ tp1/build/dist/demos/cylinder-demo.js
@@ -4,13 +4,29 @@ import {Object3D, Transformation} from "../scripts/object.js";
 var canvas = document.getElementById("my-canvas");
 var gl = await new WebGL(canvas).init();
 var angle = 0;
+var inverse = true;
 function animate(t) {
   cylinder.updateTransform([
     Transformation.rotation(t, [1, 0, 1]),
     Transformation.rotation(t * 3, [0, 1, 0])
   ]);
+  let u;
+  let v;
+  const mod = (n) => {
+    return Number(n.toFixed(2)) % 1.5;
+  };
+  if (mod(t) == 0) {
+    inverse = !inverse;
+  }
+  u = inverse ? mod(t) : 1.5 - mod(t);
+  v = !inverse ? mod(t) : 1.5 - mod(t);
   cylinderSon.updateTransform([
-    Transformation.rotation(t * 6, [0, 0, 1])
+    Transformation.rotation(t * 6, [0, 0, 1]),
+    Transformation.scale([v, v, v])
+  ]);
+  cylinderSon2.updateTransform([
+    Transformation.rotation(-t * 6, [1, 0, 1]),
+    Transformation.scale([u, u, u])
   ]);
 }
 function tick() {
@@ -19,11 +35,16 @@ function tick() {
   angle += 0.01;
   animate(angle);
 }
+var son2Transforms = [
+  Transformation.scale([0.5, 0.5, 0.5]),
+  Transformation.translate([4, 0, 0])
+];
 var sonTransforms = [
   Transformation.scale([0.5, 0.5, 0.5]),
   Transformation.translate([-4, 0, 0])
 ];
 var cylinderSon = new Object3D(new Cylinder(60, 60, 1, 3), sonTransforms, []);
+var cylinderSon2 = new Object3D(new Cylinder(60, 60, 1, 3), son2Transforms, []);
 var cylinder = new Object3D(new Cylinder(60, 60, 1, 3), [], []);
-cylinder.setChildren([cylinderSon]);
+cylinder.setChildren([cylinderSon, cylinderSon2]);
 tick();
