fa348b3693e03fa41b266409c388e8f6b76ef778
gabokatta <gkatta@fi.uba.ar>
2023-04-27 22:49:54 -0300

    Add

---
+++ tp1/build/dist/scripts/geometry.js
@@ -0,0 +1,24 @@
+export function buildIndex(geometry) {
+  var indexBuffer = [];
+  const indexCalc = (i, j) => j + (geometry.cols + 1) * i;
+  for (let i = 0; i < geometry.rows; i++) {
+    for (let j = 0; j <= geometry.cols; j++) {
+      indexBuffer.push(indexCalc(i, j), indexCalc(i + 1, j));
+    }
+  }
+  geometry.index = indexBuffer;
+}
+export function buildBuffers(geometry) {
+  var rows = geometry.rows;
+  var cols = geometry.cols;
+  for (var i = 0; i <= rows; i++) {
+    for (var j = 0; j <= cols; j++) {
+      var alfa = i / rows;
+      var beta = j / cols;
+      var p = geometry.getPosition(alfa, beta);
+      geometry.position.push(...p);
+      var n = geometry.getNormals(alfa, beta);
+      geometry.normal.push(...n);
+    }
+  }
+}
