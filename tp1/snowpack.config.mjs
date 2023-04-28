190d90a24c5be5489d137307dab4b1a0cc53b6ed
gabokatta <gkatta@fi.uba.ar>
2023-04-27 23:25:54 -0300

    Reverted: workflow changes

--- tp1/snowpack.config.mjs
+++ tp1/snowpack.config.mjs
@@ -28,9 +28,6 @@ export default {
     /* ... */
   },
   buildOptions: {
-    // put the build files in /docs
-    out: 'docs',
-    // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
-    metaUrlPath: 'snowpack'
+    /* ... */
   },
 };
