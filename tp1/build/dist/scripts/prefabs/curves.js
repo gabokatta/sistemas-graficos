a1764207ebfddf4de6f42349562d93b4f0d159a3
Snowpack Build <actions@github.com>
2023-04-29 00:15:57 +0000

    Commit from GitHub Actions (Snowpack)

---
+++ tp1/build/dist/scripts/prefabs/curves.js
@@ -0,0 +1,42 @@
+export class Bezier {
+  constructor(controlPoints, segments) {
+    this.controlPoints = controlPoints;
+    this.segments = segments;
+  }
+  getPoint() {
+    throw new Error("Method not implemented.");
+  }
+  getNormal() {
+    throw new Error("Method not implemented.");
+  }
+  getBiNormal() {
+    throw new Error("Method not implemented.");
+  }
+  getTangent() {
+    throw new Error("Method not implemented.");
+  }
+  getLength() {
+    throw new Error("Method not implemented.");
+  }
+}
+export class Spline {
+  constructor(controlPoints, segments) {
+    this.controlPoints = controlPoints;
+    this.segments = segments;
+  }
+  getPoint() {
+    throw new Error("Method not implemented.");
+  }
+  getNormal() {
+    throw new Error("Method not implemented.");
+  }
+  getBiNormal() {
+    throw new Error("Method not implemented.");
+  }
+  getTangent() {
+    throw new Error("Method not implemented.");
+  }
+  getLength() {
+    throw new Error("Method not implemented.");
+  }
+}
